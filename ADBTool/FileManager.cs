2f527e2059103df09c7fd50ba63e5e834d3cf5c4
Andy Cheung <win120a@users.noreply.github.com>
2019-09-09 20:36:49 +0800

    Add license terms.

--- ADBTool/FileManager.cs
+++ ADBTool/FileManager.cs
@@ -1,14 +1,29 @@
-﻿using AC.AndroidUtils.ADB;
+﻿/*
+    This is a part of AndroidUtils.
+
+    Copyright (C) 2011-2019 Andy Cheung
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+using AC.AndroidUtils.ADB;
 using AC.AndroidUtils.Shared;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.IO;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace AC.AndroidUtils.GUI
@@ -28,7 +43,7 @@ namespace AC.AndroidUtils.GUI
         private void FileManager_Load(object sender, EventArgs e)
         {
             fileView.Nodes.Add("/");
-            
+
 
             ShellResponse sr = adbi.RunCommand(device, "cd /; ls", false);
 
@@ -53,7 +68,7 @@ namespace AC.AndroidUtils.GUI
 
             TreeNode node = n;
             pathL.Add(n.Text);
-            while((node = node.Parent) != null)
+            while ((node = node.Parent) != null)
             {
                 pathL.Add(node.Text);
             }
